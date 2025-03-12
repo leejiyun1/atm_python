c5491922e953ba5c5ef1a7879592fc2a61a22eef
“leejiyun1” <poi20701556@gmail.com>
2025-03-12 18:04:22 +0900

    ✨ 입금기능추가
    입금기능 추가 했습니다.

--- atm.py
+++ atm.py
@@ -5,7 +5,14 @@ while True:
     num = input("사용하실 서비스 번호를 입력해주세요.(1.입금, 2.출금 3.영수증 보기 4.종료)")
 
     if num == "1":
-        print("입금기능입니다.")
+        print("입금기능입니다.")  
+        deposit_amount = input("입금하실 금액을 입력해주세요")
+        if deposit_amount.isdigit() and int(deposit_amount) > 0 :
+            balance += int(deposit_amount)
+            print(f"{deposit_amount}을 입금했고 잔액은 {balance}입니다.")
+
+        else:
+            print("정신차려 이 각박한 세상속에서!")
 
     if num == "2":
         pass
