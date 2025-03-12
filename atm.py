cb8ab4ce89330d5b515ec20ecd4099a8b697a8f1
“leejiyun1” <poi20701556@gmail.com>
2025-03-12 17:26:45 +0900

    🌱 프로젝트 초기화
    초기 파일과 기본 구조를 설정, balance 변수를 초기화하고, 사용자입력을 받을 수 있는 루프 추가, 4번 서비스 종료 기능 구현

---
+++ atm.py
@@ -0,0 +1,19 @@
+balance = 3000
+
+
+while True:
+    num = input("사용하실 서비스 번호를 입력해주세요.(1.입금, 2.출금 3.영수증 보기 4.종료)")
+
+    if num == "1":
+        print("입금기능입니다.")
+
+    if num == "2":
+        pass
+
+    if num == "3":
+        pass
+
+    if num == "4":
+        print("종료합니다.")
+        break
+
